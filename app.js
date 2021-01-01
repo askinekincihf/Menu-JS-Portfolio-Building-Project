9b1761825a8b4717de5c1080ab8a53d75fe488d6
askinekinci <askinekincihf@gmail.com>
2021-01-01 16:51:16 +0000

    added filterBtns function

--- app.js
+++ app.js
@@ -71,14 +71,47 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 36.99,
+    img: "./images/item-1.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
 
 const sectionCenter = document.querySelector(".section-center");
+const filterBtns = document.querySelectorAll(".filter-btn");
 
+// load items
 window.addEventListener("DOMContentLoaded", () => {
-  displayMenuItems(menu)
+  displayMenuItems(menu);
+
+
 });
 
+// filter items
+
+filterBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter((menuItem) => {
+      // console.log(menuItem.category);
+      if (menuItem.category === category) {
+        return menuItem;
+      }
+    });
+    if (category === "all") {
+      displayMenuItems(menu);
+    }
+    else {
+      displayMenuItems(menuCategory);
+    }
+
+    // console.log(menuCategory);
+  });
+});
 
 displayMenuItems = (menuItems) => {
 
@@ -100,5 +133,4 @@ displayMenuItems = (menuItems) => {
   })
   displayMenu = displayMenu.join("");
   sectionCenter.innerHTML = displayMenu;
-
 } 
